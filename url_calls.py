ddf4d7a9c583a0e0b796e376fa7dee6c61d0fce8
Keiran Beaton <keiranbeaton@gmail.com>
2025-03-25 19:02:34 -0700

    Fixed date time errors for 3 weeks ago split, added wrapper functions that pass correct dates into stat getter

--- url_calls.py
+++ url_calls.py
@@ -1,4 +1,5 @@
 """Module for opening webpages with selenium and returning data from therein"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,21 +8,33 @@ from datetime import datetime, timedelta
 
 from url_builders import fg_splits_page
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(20)
 
-def get_full_season(split):
+def get_stat_dicts(start_date, end_date, split):
     """Function that gets full season stats and returns a Stats tuple for each team listed."""
-
-    url = fg_splits_page("2025-03-01", "2025-11-01", f"{split}")
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(20)
+    url = fg_splits_page(start_date, end_date, f"{split}")
     driver.get(url)
     return_array = []
     try:
-        rows = driver.find_element(By.CLASS_NAME, "fg-data-grid").find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+        rows = (
+            driver.find_element(By.CLASS_NAME, "fg-data-grid")
+            .find_element(By.TAG_NAME, "tbody")
+            .find_elements(By.TAG_NAME, "tr")
+        )
         for r in rows:
             stats = r.find_elements(By.TAG_NAME, "td")
-            #Team name is stats[2], BB% is 4, K% is 5, OPS is 10, BABIP is 12, WRC+ is 16
-            return_array.append({"name": stats[2].text, "walk_rate": float(stats[4].text.replace("%","")),"k_rate": float(stats[5].text.replace("%","")), "ops": float(stats[10].text), "babip": float(stats[12].text),"wrc": int(stats[16].text)})
+            # Team name is stats[2], BB% is 4, K% is 5, OPS is 10, BABIP is 12, WRC+ is 16
+            return_array.append(
+                {
+                    "name": stats[2].text,
+                    "walk_rate": float(stats[4].text.replace("%", "")),
+                    "k_rate": float(stats[5].text.replace("%", "")),
+                    "ops": float(stats[10].text),
+                    "babip": float(stats[12].text),
+                    "wrc": int(stats[16].text),
+                }
+            )
     except Exception as e:
         print("Hit an exception", e)
     driver.close()
@@ -29,11 +42,23 @@ def get_full_season(split):
     return return_array
 
 
-def get_last_three(split) :
-    """Function that gets splits for the last three weeks"""
-    today = datetime.today().strftime("%Y-%m-%d")
-    three_weeks_ago = (datetime.today - timedelta(weeks=3)).strftime("%Y-%m-%d")
-    
-    
+def get_full_season(split):
+    """Function that takes a split as an argument and passes it into the stat
+    dict with the full season dates and returns the array from stat dict
+    getter.
+    """
 
-get_last_three("")
+    dict_array = get_stat_dicts("2025-03-01", "2025-11-01", split)
+    return dict_array
+
+
+def get_three_weeks(split):
+    """Function that takes a split as an argument and passes it into the stat
+    dict getter with the dates from the last three weeks. Returns the array
+    from the stat dict getter.
+    """
+
+    start = datetime.today().strftime("%Y-%m-%d")
+    end = (datetime.today() - timedelta(weeks=3)).strftime("%Y-%m-%d")
+    dict_array = get_stat_dicts(end, start, split)
+    return dict_array
